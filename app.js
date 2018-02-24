625bc5894fd1a13603c72a8c3fa2c3829d963ebb
Brain Tucker <brian.briantucker@gmail.com>
2018-02-24 12:58:12 -0600

    added two routes and started server on port 3000

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 var express = require('express');
+
+var app = express();
+
+//Routes
+
+//Home
+app.get('/', function(req, res){
+  res.send("this is the response from the server saying it got the request");
+});
+
+//Luke
+app.get('/luke', function(req, res){
+  res.send("this is a server response on the Luke page");
+});
+
+app.listen(3000, function(){
+  console.log("The app is running on port 3000.");
+});

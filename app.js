12580a10c7cdc0909292800cd75226748e481b39
Brain Tucker <brian.briantucker@gmail.com>
2018-02-25 11:32:55 -0600

    prepping application for production

--- app.js
+++ app.js
@@ -26,6 +26,5 @@ app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 //notFound
 app.get('*', routes.notFound);
 
-app.listen(3000, function(){
-  console.log("The app is running on port 3000.");
-});
+
+app.listen(process.env.PORT || 3000);

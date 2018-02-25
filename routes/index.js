acb2585d01ed2d1735b9c9f75885bb3b6d86e781
Brain Tucker <brian.briantucker@gmail.com>
2018-02-25 10:59:51 -0600

    main charcters are now displayed for each movie on movie_single template

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,14 @@ exports.movie_single =  function(req, res){
   if (episode_number >= 1 && episode_number <= 6) {
       var movie = movies[episode_number-1];
       var title = movie.title;
+      var main_characters = movie.main_characters;
 
     res.render('movie_single', {
+      //left variable accessable in our template file: right, what it represents
       title: title,
       movies: movies,
-      movie: movie
+      movie: movie,
+      main_characters: main_characters
     });
   } else {
     res.send("this is not the page you are looking for");
